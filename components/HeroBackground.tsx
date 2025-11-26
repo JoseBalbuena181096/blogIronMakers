ef969c73f8ade85c61640dfa7c13b24b6edef78d
Jose Balbuena <josebalbuena181096@gmail.com>
2025-11-26 17:54:04 -0600

    fix: change to pure blue colors, remove cyan/green tint

--- components/HeroBackground.tsx
+++ components/HeroBackground.tsx
@@ -6,11 +6,11 @@ const HeroBackground = () => {
             {/* Base gradient - azul oscuro */}
             <div className="absolute inset-0 bg-gradient-to-b from-gray-900 via-blue-950/30 to-gray-900" />
 
-            {/* Multiple wave layers - azul blanquecino */}
+            {/* Multiple wave layers - azul puro */}
             <div className="absolute top-1/4 left-0 w-[600px] h-[600px] bg-blue-400 rounded-full mix-blend-screen filter blur-[120px] opacity-30 animate-wave-flow-1" />
-            <div className="absolute top-1/3 left-0 w-[500px] h-[500px] bg-cyan-300 rounded-full mix-blend-screen filter blur-[100px] opacity-25 animate-wave-flow-2" />
-            <div className="absolute top-1/2 left-0 w-[700px] h-[400px] bg-blue-300 rounded-full mix-blend-screen filter blur-[140px] opacity-20 animate-wave-flow-3" />
-            <div className="absolute bottom-1/4 left-0 w-[550px] h-[550px] bg-cyan-400 rounded-full mix-blend-screen filter blur-[110px] opacity-25 animate-wave-flow-1" style={{ animationDelay: '3s' }} />
+            <div className="absolute top-1/3 left-0 w-[500px] h-[500px] bg-blue-300 rounded-full mix-blend-screen filter blur-[100px] opacity-25 animate-wave-flow-2" />
+            <div className="absolute top-1/2 left-0 w-[700px] h-[400px] bg-blue-500 rounded-full mix-blend-screen filter blur-[140px] opacity-20 animate-wave-flow-3" />
+            <div className="absolute bottom-1/4 left-0 w-[550px] h-[550px] bg-blue-400 rounded-full mix-blend-screen filter blur-[110px] opacity-25 animate-wave-flow-1" style={{ animationDelay: '3s' }} />
             <div className="absolute top-0 left-0 w-[450px] h-[450px] bg-blue-200 rounded-full mix-blend-screen filter blur-[90px] opacity-20 animate-wave-flow-2" style={{ animationDelay: '5s' }} />
         </div>
     );
